981032d00e02ee3e0f184fe4f8dacdf6413d7e34
Nguyen Ha Ninh <hu17@mih.vn>
2026-05-25 08:02:36 +0000

    zce_tts_chk - Odata V2 - 25.05.2025

---
+++ webapp/Component.js
@@ -0,0 +1,12 @@
+sap.ui.define(
+    ["sap/suite/ui/generic/template/lib/AppComponent"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("zcettschk.Component", {
+            metadata: {
+                manifest: "json"
+            }
+        });
+    }
+);
