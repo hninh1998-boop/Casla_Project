c61378c1f42a31c8305ae38a31a1d8936c832d34
Nguyen Ha Ninh <hu17@mih.vn>
2026-06-03 00:35:08 +0000

    zmb52 - 03.06.2026

---
+++ webapp/Component.js
@@ -0,0 +1,12 @@
+sap.ui.define(
+    ["sap/suite/ui/generic/template/lib/AppComponent"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("zmb52.Component", {
+            metadata: {
+                manifest: "json"
+            }
+        });
+    }
+);
