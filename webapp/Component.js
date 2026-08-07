4eb1119effc71442079fb1618787c9c9a568c3d3
hninh1998 <hninh1998@gmail.com>
2026-08-07 06:46:22 +0000

    zpo_giacong_ov2

---
+++ webapp/Component.js
@@ -0,0 +1,12 @@
+sap.ui.define(
+    ["sap/suite/ui/generic/template/lib/AppComponent"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("zpogiacongov2.Component", {
+            metadata: {
+                manifest: "json"
+            }
+        });
+    }
+);
